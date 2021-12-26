693d51b2cd29475857806126864e3083a892ae2d
s1m0n <laissezfu@gmail.com>
2021-12-26 16:01:05 -0600

    complete Genre Graph

---
+++ main.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+vgSales = pd.read_csv(os.path.join("vgsales.csv"))
+
+# Find out what genre of games is most popular
+fig, ax = plt.subplots(figsize=(6,4))
+genreSales = vgSales.groupby("Genre").sum().sort_values("Global_Sales",ascending=False)
+genreSales.plot.bar(ax=ax,y=["Global_Sales"])
+fig.savefig("GenreSales")
