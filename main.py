2579832f0c0b7dfbdc33d1a4f67dfa1920f32e90
s1m0n <laissezfu@gmail.com>
2021-12-26 16:35:31 -0600

    creates Data directory

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-vgSales = pd.read_csv(os.path.join("vgsales.csv"))
 
-# Find out what genre of games is most popular
-fig, ax = plt.subplots(figsize=(6,4))
-genreSales = vgSales.groupby("Genre").sum().sort_values("Global_Sales",ascending=False)
-genreSales.plot.bar(ax=ax,y=["Global_Sales"],rot=45)
-fig.savefig(os.path.join("Graphs","GenreSales"),bbox_inches='tight')
+# Read in data
+vgSales = pd.read_csv(os.path.join("Data","vgsales.csv"))
+fig, ax = plt.subplots(figsize=(6, 4))
+
+# Find out what genre of games is most popular overall
+genreSales = vgSales.groupby("Genre").sum().sort_values(
+    "Global_Sales", ascending=False)
+genreSales.plot.bar(ax=ax, y=["Global_Sales"], rot=45)
+fig.savefig(os.path.join("Graphs", "GenreSales"), bbox_inches='tight')
+
+# How do sales of different types of games change over time
+
+# 
